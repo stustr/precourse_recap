76882f853720300ca11b0b23dd3a0718d8b9de35
1919dev <sfbsturrock@gmail.com>
2022-10-17 17:05:17 +0100

    added function

--- precourse_recap.py
+++ precourse_recap.py
@@ -2,7 +2,10 @@ num = 6
 hello = 'hello world'
 print(hello)
 print(6**2)
-if (num>5):
-    print("num is",num ,"it's bigger than 5")
-else:
-    print("num is",num,"it's smaller than 5")
+def checknum():
+    if (num>5):
+        print("num is",num ,"it's bigger than 5")
+    else:
+        print("num is",num,"it's smaller than 5")
+
+checknum
